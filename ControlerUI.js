31365d4469a19d66aa8fe1a6506ad5e46addd9da
joel <joelbenitezdonari@gmail.com>
2024-05-20 17:58:47 +0200

    object update creation msg login homepage creation input reply

--- ControlerUI.js
+++ ControlerUI.js
@@ -5,8 +5,13 @@ import { MessageController } from './MessageController.js'
 
 
 export class ControllerUI{
+    
+    socket = null;
+    messageController = null;
     constructor(){
+        this.messageController = new MessageController();
         this.init();
+
     }
     init(){
         // listeners add ui 
@@ -113,8 +118,10 @@ export class ControllerUI{
 
     }
     sendNewMessage(data){
-        messageController.createMessage(data);
-        this.updateDoom();
+        this.messageController.createMessage(data); // to doom
+        this.updateDoom(); // update doom
+        this.sendMessage(data); // to backend
+
     }
     juicyBtn() {
         let selectors = [
@@ -159,6 +166,9 @@ export class ControllerUI{
 
     loginInitiate(){
         let _me = this;
+
+        _me.simulationChat();
+
         const userSelectionImg = document.querySelector(".selectionUserImage");
         //default selection
         let userImgSelected = "user_1"
@@ -213,7 +223,6 @@ export class ControllerUI{
 
 
     }
-    socket = null;
     async initiConectionBack(){
        
         const statusElement = document.getElementById("contextJoin");
@@ -239,6 +248,7 @@ export class ControllerUI{
                     console.log('Conexión WebSocket abierta:', event);
                     // Enviar un mensaje al servidor
                     this.socket.send('Hola servidor!');
+                    this.hideHomePgae()
                     resolve();
                 };
 
@@ -251,6 +261,11 @@ export class ControllerUI{
             // Configurar los manejadores de eventos restantes después de que la conexión se haya abierto
             this.socket.onmessage = (event) => {
                 console.log('Mensaje recibido del servidor:', event.data);
+                if(typeof event.data != "string"){
+                    this.messageController.createMessage(data); // to doom
+                    this.updateDoom(); // update doom
+                }
+                
             };
 
             this.socket.onclose = (event) => {
@@ -277,6 +292,12 @@ export class ControllerUI{
         }
     }
 
+    hideHomePgae(){
+        const HomePageElement = document.querySelector(".homePage");
+
+        HomePageElement.style.display = "none";
+    }
+
     // Método para enviar un mensaje a través del WebSocket
     sendMessage(message) {
         if (this.socket && this.socket.readyState === WebSocket.OPEN) {
@@ -286,37 +307,241 @@ export class ControllerUI{
         }
     }
 
+    NewChat(){
+
+    }
+
+    simulationChat(){
+
+        this.messageController.createMessage({
+            id: "10001",
+            imageUser: "./user_5.png",
+            user: "Alice",
+            time: "10:00",
+            msg: "Hey everyone!",
+            type: "message",
+            ContainReactiosn: true,
+            reactions: {
+                like: {
+                    type: "Like",
+                    iconClass: "icon-like",
+                    usersReactions: [
+                        {
+                            user: "Bob",
+                            urlImg: "./user_2.png"
+                        },
+                        {
+                            user: "Charlie",
+                            urlImg: "./user_3.png"
+                        }
+                    ]
+                }
+            }
+        });
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10002",
+                imageUser: "./user_2.png",
+                user: "Bob",
+                time: "10:01",
+                msg: "Hey Alice! How are you?",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 1000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10003",
+                imageUser: "./user_3.png",
+                user: "Charlie",
+                time: "10:02",
+                msg: "Hi Alice and Bob!",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 2000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10004",
+                imageUser: "./user_5.png",
+                user: "Alice",
+                time: "10:03",
+                msg: "I'm good, thanks! How about you guys?",
+                type: "message",
+                ContainReactiosn: true,
+                reactions: {
+                    like: {
+                        type: "Like",
+                        iconClass: "icon-like",
+                        usersReactions: [
+                            {
+                                user: "Bob",
+                                urlImg: "./user_2.png"
+                            }
+                        ]
+                    }
+                }
+            });
+        }, 3000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10005",
+                imageUser: "./user_2.png",
+                user: "Bob",
+                time: "10:04",
+                msg: "I'm great! Just working on some projects.",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 4000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10006",
+                imageUser: "./user_3.png",
+                user: "Charlie",
+                time: "10:05",
+                msg: "Same here. Busy but good!",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 5000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10007",
+                imageUser: "./user_5.png",
+                user: "Alice",
+                time: "10:06",
+                msg: "Nice to hear! Any plans for the weekend?",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 6000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10008",
+                imageUser: "./user_2.png",
+                user: "Bob",
+                time: "10:07",
+                msg: "Thinking of going hiking. What about you, Charlie?",
+                type: "responseMessage",
+                response: {
+                    user: "Alice",
+                    msg: "Nice to hear! Any plans for the weekend?"
+                },
+                ContainReactiosn: false,
+            });
+        }, 7000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10009",
+                imageUser: "./user_3.png",
+                user: "Charlie",
+                time: "10:08",
+                msg: "Might go for a run and then relax at home.",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 8000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10010",
+                imageUser: "./user_5.png",
+                user: "Alice",
+                time: "10:09",
+                msg: "Sounds good! Maybe we can catch up on Monday.",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 9000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10011",
+                imageUser: "./user_2.png",
+                user: "Bob",
+                time: "10:10",
+                msg: "Sure, let's do that.",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 10000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10012",
+                imageUser: "./user_3.png",
+                user: "Charlie",
+                time: "10:11",
+                msg: "Have a great weekend, guys!",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 11000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10013",
+                imageUser: "./user_5.png",
+                user: "Alice",
+                time: "10:12",
+                msg: "You too, Charlie!",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 12000);
+        
+        setTimeout(() => {
+            this.messageController.createMessage({
+                id: "10014",
+                imageUser: "./user_2.png",
+                user: "Bob",
+                time: "10:13",
+                msg: "Take care!",
+                type: "message",
+                ContainReactiosn: false,
+            });
+        }, 13000);
+    }
+
     
 }
 
 
-// Ejemplo de uso
-const messageController = new MessageController();
 
-// messageController.createMessage({
+
+// messagecontroller.createmessage({
 //     id: "12345",
-//     imageUser: "./user_1.png",
-//     user: "Kevin Fragment",
+//     imageuser: "./user_1.png",
+//     user: "kevin fragment",
 //     time: "9:23",
-//     msg: "Holaaaaaaaaa",
+//     msg: "holaaaaaaaaa",
 //     type: "message",
-//     ContainReactiosn: true,
+//     containreactiosn: true,
 //     reactions: {
 //         like: {
-//             type: "Like",
-//             iconClass: "icon-like",
-//             usersReactions: [
+//             type: "like",
+//             iconclass: "icon-like",
+//             usersreactions: [
 //                 {
 //                     user: "jhon",
-//                     urlImg: "./user_2.png"
+//                     urlimg: "./user_2.png"
 //                 },
 //                 {
 //                     user: "remei",
-//                     urlImg: "./user_3.png"
+//                     urlimg: "./user_3.png"
 //                 },
 //                 {
 //                     user: "victor",
-//                     urlImg: "./user_4.png"
+//                     urlimg: "./user_4.png"
 //                 }
                 
 //             ]
@@ -432,205 +657,5 @@ let data = {
     }
 }
 //messageController.updateReaction(data)
-function simulationChat(){
 
-    messageController.createMessage({
-        id: "10001",
-        imageUser: "./user_5.png",
-        user: "Alice",
-        time: "10:00",
-        msg: "Hey everyone!",
-        type: "message",
-        ContainReactiosn: true,
-        reactions: {
-            like: {
-                type: "Like",
-                iconClass: "icon-like",
-                usersReactions: [
-                    {
-                        user: "Bob",
-                        urlImg: "./user_2.png"
-                    },
-                    {
-                        user: "Charlie",
-                        urlImg: "./user_3.png"
-                    }
-                ]
-            }
-        }
-    });
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10002",
-            imageUser: "./user_2.png",
-            user: "Bob",
-            time: "10:01",
-            msg: "Hey Alice! How are you?",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 1000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10003",
-            imageUser: "./user_3.png",
-            user: "Charlie",
-            time: "10:02",
-            msg: "Hi Alice and Bob!",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 2000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10004",
-            imageUser: "./user_5.png",
-            user: "Alice",
-            time: "10:03",
-            msg: "I'm good, thanks! How about you guys?",
-            type: "message",
-            ContainReactiosn: true,
-            reactions: {
-                like: {
-                    type: "Like",
-                    iconClass: "icon-like",
-                    usersReactions: [
-                        {
-                            user: "Bob",
-                            urlImg: "./user_2.png"
-                        }
-                    ]
-                }
-            }
-        });
-    }, 3000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10005",
-            imageUser: "./user_2.png",
-            user: "Bob",
-            time: "10:04",
-            msg: "I'm great! Just working on some projects.",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 4000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10006",
-            imageUser: "./user_3.png",
-            user: "Charlie",
-            time: "10:05",
-            msg: "Same here. Busy but good!",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 5000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10007",
-            imageUser: "./user_5.png",
-            user: "Alice",
-            time: "10:06",
-            msg: "Nice to hear! Any plans for the weekend?",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 6000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10008",
-            imageUser: "./user_2.png",
-            user: "Bob",
-            time: "10:07",
-            msg: "Thinking of going hiking. What about you, Charlie?",
-            type: "responseMessage",
-            response: {
-                user: "Alice",
-                msg: "Nice to hear! Any plans for the weekend?"
-            },
-            ContainReactiosn: false,
-        });
-    }, 7000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10009",
-            imageUser: "./user_3.png",
-            user: "Charlie",
-            time: "10:08",
-            msg: "Might go for a run and then relax at home.",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 8000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10010",
-            imageUser: "./user_5.png",
-            user: "Alice",
-            time: "10:09",
-            msg: "Sounds good! Maybe we can catch up on Monday.",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 9000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10011",
-            imageUser: "./user_2.png",
-            user: "Bob",
-            time: "10:10",
-            msg: "Sure, let's do that.",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 10000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10012",
-            imageUser: "./user_3.png",
-            user: "Charlie",
-            time: "10:11",
-            msg: "Have a great weekend, guys!",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 11000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10013",
-            imageUser: "./user_5.png",
-            user: "Alice",
-            time: "10:12",
-            msg: "You too, Charlie!",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 12000);
-    
-    setTimeout(() => {
-        messageController.createMessage({
-            id: "10014",
-            imageUser: "./user_2.png",
-            user: "Bob",
-            time: "10:13",
-            msg: "Take care!",
-            type: "message",
-            ContainReactiosn: false,
-        });
-    }, 13000);
-}
 
-simulationChat();
